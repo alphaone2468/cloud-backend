64696003d87c24f534b7e2e9266f5a5d99da1b3b
Ahmed <129504534+alphaone2468@users.noreply.github.com>
2024-08-03 02:45:12 +0530

    Update uploadController.js

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -3,12 +3,17 @@ const data = require("../models/dataSchema");
 
 exports.uploadData = async (req, res) => {
     try {
+        console.log("gine");
         const fileImg = req.body.stringimg;
+        console.log("gine");
         const fileVid = req.body.stringvideo;
+        console.log("gine");
 
+        console.log("gine");
         const uploadResponseimg = await cloudinary.uploader.upload(fileImg, {
             upload_preset: 'images',
         });
+        console.log("gine");
         const uploadResponsevid = await cloudinary.uploader.upload(fileVid, {
             resource_type: 'video',
             upload_preset: 'videos',
@@ -20,10 +25,10 @@ exports.uploadData = async (req, res) => {
             image: uploadResponseimg.secure_url,
             video: uploadResponsevid.secure_url
         };
-
+        console.log("fgfdedfv");
         let result = await data(obj);
         result = await result.save();
-        res.json({ msg: [uploadResponseimg.url, uploadResponsevid.url] });
+        res.json({ msg: [uploadResponseimg.secure_url, uploadResponsevid.secure_url] });
     } catch (err) {
         console.error(err);
         res.status(500).json({ err: 'Something went wrong' });
