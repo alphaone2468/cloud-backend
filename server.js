8ef2f877e4866ce7b9b3a420735537480c5762c3
Ahmed <129504534+alphaone2468@users.noreply.github.com>
2024-08-03 03:10:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ const videoController = require("./controllers/videoController");
 const dataController = require("./controllers/dataController");
 const PORT = process.env.PORT
 const app = express();
+// https://neon-flask-enterprises-task.netlify.app
 app.use(cors());
 app.use(cors({
-  origin: 'https://neon-flask-enterprises-task.netlify.app' // Replace with the origin you want to allow
+  origin: 'http://localhost:5173' // Replace with the origin you want to allow
 }));
 app.use(bodyParser.json({ limit: '100mb' }));
 app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
